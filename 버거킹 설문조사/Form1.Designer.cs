95526c37cefbfe873c702dba4b83b0b29a0d9443
thdwoqor <celebarte01@gmail.com>
2021-07-16 10:40:15 +0900

    beta1

---
+++ 버거킹 설문조사/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace 버거킹_설문조사
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bunifuTileButton1 = new ns1.BunifuTileButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(367, 16);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(21, 21);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "0";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(310, 16);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(41, 21);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "000";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(254, 16);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(41, 21);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "000";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(198, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(41, 21);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "010";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(135)))), ((int)(((byte)(50)))));
+            this.textBox2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(143, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(41, 21);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "492";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "000";
+            // 
+            // bunifuTileButton1
+            // 
+            this.bunifuTileButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(135)))), ((int)(((byte)(50)))));
+            this.bunifuTileButton1.color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(135)))), ((int)(((byte)(50)))));
+            this.bunifuTileButton1.colorActive = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(135)))), ((int)(((byte)(50)))));
+            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton1.Image = null;
+            this.bunifuTileButton1.ImagePosition = 14;
+            this.bunifuTileButton1.ImageZoom = 50;
+            this.bunifuTileButton1.LabelPosition = 30;
+            this.bunifuTileButton1.LabelText = "시작";
+            this.bunifuTileButton1.Location = new System.Drawing.Point(88, 48);
+            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.bunifuTileButton1.Name = "bunifuTileButton1";
+            this.bunifuTileButton1.Size = new System.Drawing.Size(138, 37);
+            this.bunifuTileButton1.TabIndex = 12;
+            this.bunifuTileButton1.Click += new System.EventHandler(this.bunifuTileButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(297, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(353, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "-";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.bunifuTileButton1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(10, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 100);
+            this.panel1.TabIndex = 19;
+            this.panel1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(12, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(394, 82);
+            this.panel2.TabIndex = 20;
+            this.panel2.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(24, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 30);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "확인코드:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 12);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "참여해 주셔서 감사합니다.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(281, 12);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "주황색 부분 이외에는 기입하지 않아도 무방합니다.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(419, 141);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Burger King";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private ns1.BunifuTileButton bunifuTileButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 버거킹 설문조사/Form1.cs
@@ -0,0 +1,164 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 버거킹_설문조사
+{
+    public partial class Form1 : Form
+    {
+        private ChromeDriverService _driverService = null;
+        private ChromeOptions _options = null;
+        private ChromeDriver _driver = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.Visible = true;
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("--headless");
+            _options.AddArgument("disable-gpu");
+            _options.AddArgument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)" + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 3) // 이벤트 핸들러 설정된 컨트롤의 글자입력수가 3글자이면,
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 3) // 이벤트 핸들러 설정된 컨트롤의 글자입력수가 3글자이면,
+            {
+                textBox3.Focus();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length == 3) // 이벤트 핸들러 설정된 컨트롤의 글자입력수가 3글자이면,
+            {
+                textBox4.Focus();
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length == 3) // 이벤트 핸들러 설정된 컨트롤의 글자입력수가 3글자이면,
+            {
+                textBox5.Focus();
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Length == 3) // 이벤트 핸들러 설정된 컨트롤의 글자입력수가 3글자이면,
+            {
+                textBox6.Focus();
+            }
+        }
+
+        public void start()
+        {
+            _driver = new ChromeDriver(@"chrom", _options);
+            //_driver.Navigate().GoToUrl("https://kor.tellburgerking.com/");  // 웹 사이트에 접속합니다.
+            _driver.Navigate().GoToUrl("https://kor.tellburgerking.com/?AspxAutoDetectCookieSupport=1");  // 웹 사이트에 접속합니다.
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            //_driver.Manage().Window.Position = new System.Drawing.Point(1, 1);
+
+            var element = _driver.FindElement(By.XPath("//*[@id='NextButton']"));
+            _driver.ExecuteScript("arguments[0].click();", element);
+
+            element = _driver.FindElement(By.XPath("//*[@id='CN1']"));
+            element.SendKeys(textBox1.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='CN2']"));
+            element.SendKeys(textBox2.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='CN3']"));
+            element.SendKeys(textBox3.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='CN4']"));
+            element.SendKeys(textBox4.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='CN5']"));
+            element.SendKeys(textBox5.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='CN6']"));
+            element.SendKeys(textBox6.Text);
+            element = _driver.FindElement(By.XPath("//*[@id='NextButton']"));
+            _driver.ExecuteScript("arguments[0].click();", element);
+
+            bool check = true;
+            while (check)
+            {
+                if (InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        try
+                        {
+                            element = _driver.FindElement(By.XPath("//*[@id='NextButton']"));
+                            _driver.ExecuteScript("arguments[0].click();", element);
+                        }
+                        catch (Exception ex)
+                        {
+                            panel1.Visible = false;
+
+                            //element = _driver.FindElement(By.XPath("//*[@id='finishIncentiveHolder']/p[3]"));
+                            //label6.Text = element.Text;
+                            string source = _driver.PageSource;
+                            string data = Regex.Split(Regex.Split(source, @"ValCode"">")[1], @"</p>")[0];
+                            label6.Text = data;
+                            panel2.Visible = true;
+                            check = false;
+                        }
+
+                    }));
+                }
+                else
+                {
+                    try
+                    {
+                        element = _driver.FindElement(By.XPath("//*[@id='NextButton']"));
+                        _driver.ExecuteScript("arguments[0].click();", element);
+                    }
+                    catch (Exception ex)
+                    {
+                        panel1.Visible = false;
+
+                        //element = _driver.FindElement(By.XPath("//*[@id='finishIncentiveHolder']/p[3]"));
+                        //label6.Text = element.Text;
+
+                        string source = _driver.PageSource;
+                        string data = Regex.Split(Regex.Split(source, @"ValCode"">")[1], @"</p>")[0];
+                        label6.Text = data;
+
+                        panel2.Visible = true;
+                        check = false;
+                    }
+                }
+            }
+        }
+
+        private void bunifuTileButton1_Click(object sender, EventArgs e)
+        {
+            Thread acceptThread = new Thread(() => start());
+            acceptThread.IsBackground = true;   // 부모 종료시 스레드 종료
+            acceptThread.Start();
+        }
+
+    }
+}
